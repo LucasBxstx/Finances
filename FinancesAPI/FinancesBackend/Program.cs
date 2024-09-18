839f6685bf3332df8ff5905ac6feaac2893156a5
LucasBxstx <lucasbeste8@gmail.com>
2024-09-18 15:56:48 +0200

    backend getUserId when login and JWTAuth

--- FinancesAPI/FinancesBackend/Program.cs
+++ FinancesAPI/FinancesBackend/Program.cs
@@ -1,9 +1,13 @@
 using FinancesBackend;
 using FinancesBackend.Common.Exceptions;
+using FinancesBackend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 ConfigureMvc(builder.Services);
 ConfigureSwagger(builder.Services);
 ConfigureDbContext(builder.Services, builder.Configuration);
-ConfigureAuthorization(builder.Services);
+ConfigureAuthorization(builder.Services, builder.Configuration);
 ConfigureServices(builder.Services);
 
 var app = builder.Build();
@@ -29,10 +33,33 @@ static void ConfigureDbContext(IServiceCollection services, IConfiguration confi
     });
 }
 
-static void ConfigureAuthorization(IServiceCollection services)
+static void ConfigureAuthorization(IServiceCollection services, IConfiguration configuration)
 {
-    services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
-    services.AddAuthorizationBuilder();
+    // JWT Authentication configuration
+    var jwtSettings = configuration.GetSection("Jwt");
+    var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
+
+    services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            ClockSkew = TimeSpan.Zero // Token expiration time precision
+        };
+    });
+
+    services.AddAuthorization();
     services.AddIdentityCore<IdentityUser>()
         .AddEntityFrameworkStores<FinancesContext>()
         .AddApiEndpoints();
@@ -52,12 +79,12 @@ static void ConfigureMvc(IServiceCollection services)
     services.AddCors();
 }
 
-static void ConfigureSwagger(IServiceCollection services)
+/*static void ConfigureSwagger(IServiceCollection services)
 {
     services.AddEndpointsApiExplorer();
     services.AddSwaggerGen(options =>
     {
-        options.AddSecurityDefinition("oauth2", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
         {
             In = ParameterLocation.Header,
             Name = "Authorization",
@@ -65,12 +92,33 @@ static void ConfigureSwagger(IServiceCollection services)
         });
 
         options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+        // Weitere Swagger-Konfigurationsoptionen
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Finances API", Version = "v1" });
+    });
+}*/
+static void ConfigureSwagger(IServiceCollection services)
+{
+    services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo { Title = "Finances API", Version = "v1" });
+
+        // Entferne Konflikte durch Ignorieren bestimmter Typen
+        options.IgnoreObsoleteActions();
+        options.IgnoreObsoleteProperties();
+
+        // Füge Unterstützung für andere Formate hinzu, wenn notwendig
+        options.DescribeAllParametersInCamelCase();
+        options.CustomSchemaIds(type => type.FullName); // Vermeidet Namenskonflikte
     });
 }
+
 
 static void ConfigureServices(IServiceCollection services)
 {
     services.AddSingleton<WrappedDbUpdateConcurrencyExceptionFactory>();
+
+    services.AddScoped<IJwtTokenService, JwtTokenService>();
 }
 
 static void ConfigureApp(WebApplication webApplication, IConfiguration configuration)
@@ -82,6 +130,7 @@ static void ConfigureApp(WebApplication webApplication, IConfiguration configura
     webApplication.MapControllers();
 
     webApplication.MapIdentityApi<IdentityUser>();
+    webApplication.UseAuthentication();
     webApplication.UseAuthorization();
 
 
