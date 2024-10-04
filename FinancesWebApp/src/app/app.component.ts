238eab005c1effc4c18c1ce63a1b82d00ead4504
LucasBxstx <lucasbeste8@gmail.com>
2024-10-05 00:36:01 +0200

    Cookie consent banner

--- FinancesWebApp/src/app/app.component.ts
+++ FinancesWebApp/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TranslocoService } from '@ngneat/transloco';
+import { CookieBannerComponent } from './shared/components/cookie-banner/cookie-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CookieBannerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
